5ecbf122c62de4127e2d6c3ef741970a99de039e
Billbundtechnology-design <Billbundtechnology@gmail.com>
2026-09-12 16:30:37 -0700

    new update

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import '../config.js'  // Load API configuration from environment variables
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
